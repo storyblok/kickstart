1c2737d36ecba37207a2e045db206b189f2c1262
Dominik Angerer <dominikangerer1@gmail.com>
2017-09-04 10:01:45 +0200

    adds component collection
    you can use the CLI to import those components

---
+++ components-v2.js
@@ -0,0 +1,48 @@
+{
+  "components": [
+    {
+      "name": "column",
+      "display_name": null,
+      "created_at": "2017-09-04T07:52:43.893Z",
+      "id": 47598,
+      "schema": "{\"body\":{\"type\":\"bloks\"},\"width\":{\"type\":\"option\",\"source\":\"external\",\"external_datasource\":\"https://raw.githubusercontent.com/storyblok/default-datasources/master/bootstrap-md-grid.json\"}}",
+      "image": null,
+      "preview_field": null,
+      "is_root": false,
+      "is_nestable": true
+    },
+    {
+      "name": "grid",
+      "display_name": null,
+      "created_at": "2017-09-04T07:52:31.398Z",
+      "id": 47597,
+      "schema": "{\"columns\":{\"type\":\"bloks\"}}",
+      "image": null,
+      "preview_field": null,
+      "is_root": false,
+      "is_nestable": true
+    },
+    {
+      "name": "root",
+      "display_name": null,
+      "created_at": "2017-09-04T07:00:08.991Z",
+      "id": 47595,
+      "schema": "{\"body\":{\"type\":\"bloks\"}}",
+      "image": null,
+      "preview_field": null,
+      "is_root": true,
+      "is_nestable": false
+    },
+    {
+      "name": "teaser",
+      "display_name": null,
+      "created_at": "2017-09-04T07:15:48.662Z",
+      "id": 47596,
+      "schema": "{\"headline\":{\"type\":\"text\"}}",
+      "image": null,
+      "preview_field": null,
+      "is_root": false,
+      "is_nestable": true
+    }
+  ]
+}
